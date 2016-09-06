e168d943ff05e6815d53929fd2afcc3ffcefef90
Eddy Leon <Eddy Leon>
2016-09-06 05:56:34 -0400

    working game, database is 11.11% complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,11 @@ $(document).ready(function(){
 
 		//Time to answer a question in seconds
 
-		timer: 15,
+		time: 15,
+
+		//Holder to stop timer
+
+		timer:'',
 
 		//Used to know when the trivia game is over
 
@@ -24,7 +28,23 @@ $(document).ready(function(){
 
 		//questionsArray will contain the appropiate questions for the game
 
-		questionsArray:[]
+		questionsArray:[],
+
+		//Keep track of correct and incorrect
+
+		correct:0,
+
+		incorrect:0,
+
+		correctAnswer:' ',
+
+		userAnswers:[],
+
+		toBeAdded:[],
+
+		maxQuestions: 15
+
+
 
 	};
 
@@ -58,15 +78,35 @@ $(document).ready(function(){
 
 	var q2 = new quizQuestion('geography', 2, 'easy', 'Which one of the following is the capital of South Korea?', 'Pyongyang', 'Busan', 'Incheon', 'Seoul', 'Seoul');
 
-	var q3 = new quizQuestion('geography', 3, 'easy', 'From what country did the Italian explorer Christopher Columbus depart from?', 'Portugal', 'Italy', 'Spain', 'France');
+	var q3 = new quizQuestion('geography', 3, 'easy', "Which continent occupies about 30% of the Earth's total land area?" , 'South America', 'Asia', 'Europe', 'Australia', 'Asia');
 
-	var q4 = new quizQuestion('geography', 4, 'easy', "After what Greek god did Saturn receive it's name?", 'Apollo', 'Chronus', 'Hephaestus', 'Poseidon', 'Cronus');
+	var q4 = new quizQuestion('geography', 4, 'easy', "Which imaginary line separates the north Pacific from the south pacific?", 'The Artic Circle', 'Meridial', 'Equator', 'Tropic', 'Equator');
 
-	var q5 = new quizQuestion('geography', 5, 'easy', 'Approximately, how many people passed away during the bulbonic plate?', '2 Million', '100 Thousand', '10 Million', '25 Million', '25 Million');
+	var q5 = new quizQuestion('geography', 5, 'easy', 'In 1997 this British crown colony became a special administrative region of China', 'Beijing', 'Shanghai', 'Macau', 'Hong Kong', 'Hong Kong');
+
+	var q6 = new quizQuestion('geography', 6, 'easy', "Whats is the largest country in the world that has a red, white & blue flag?", 'Paraguay', 'Russia', 'Croatia', 'Norway', 'Russia');
+
+	var q7 = new quizQuestion('geography', 7, 'easy', 'Name of the island shared by England, Wales & Scotland', 'Great Britain', 'Isles of Scilly', 'Isle of Wight', 'Throny Island', 'Great Britain');
+
+	var q8 = new quizQuestion('geography', 8, 'easy', "It's the only papal state left in the world", 'Verona', 'Rome', 'Vatican City', 'Venice', 'Vatican City');
+
+	var q9 = new quizQuestion('geography', 9, 'easy', "Besides North America, it's the only continent completely north of the equator", 'Antartica', 'Europe', 'Asia', 'Russia', 'Europe');
+
+	var q10 = new quizQuestion('geography', 10, 'easy', 'Clocks Peru read the same as in this U.S. time zone', 'Eastern', 'Western', 'Central', 'South Western', 'Eastern');
+
+	var q11 = new quizQuestion('geography', 11, 'easy', 'What is the largest country (in land) in South America?', 'Argentina', 'Colombia', 'Brazil', 'Chile', 'Brazil');
+
+	var q12 = new quizQuestion('geography', 12, 'easy', 'What is the longest river on Earth that flows into the Mediterranean Sea?', 'Amazon', 'Yangtze River', 'Parana River', 'The Nile', 'The Nile');
+
+	var q13 = new quizQuestion('geography', 13, 'easy', 'City that stands on the ruins of Tenochtitlan, the capital of the Aztec people', 'Mexico City', 'Pueblas', 'Oaxaca', 'Veracruz', 'Mexico City');
+
+	var q14 = new quizQuestion('geography', 14, 'easy', 'What is the capital of France', 'Marseille', 'Orléans', 'Paris', 'Lyon', 'Paris');
+
+	var q15 = new quizQuestion('geography', 15, 'easy', "If you live on the equator, you'll note you usually have this many hours of daylight a day", '12 Hours', '2 Hours', '8 Hours', '20 Minutes', '12 Hours');
 
 	//Array that holds all the questions
 
-	var allQuestions = [q1, q2, q3, q4, q5];
+	var allQuestions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15];
 
 	//The function isGameSelectionComplete checks whether the game object properties have been entered and displays the button to begin the game
 
@@ -167,13 +207,126 @@ $(document).ready(function(){
 
 	});
 
+	function scoreGiver(){
+
+		var score  = Math.floor(100*(game.correct/(game.correct+game.incorrect)));
+
+		if(score >= 90){
+
+			backgroundChanger('A.jpg');
+
+			return;
+
+		}
+
+		 if(score >= 80 && score < 90){
+
+			backgroundChanger('B.jpg');
+
+			return;
+
+		}
+
+		 if(score >= 70 && score < 80){
+
+			backgroundChanger('C.jpg');
+
+			return;
+			
+		}
+
+		 if(score >= 60 && score < 70){
+
+			backgroundChanger('D.jpg');
+
+			return;
+			
+		}
+
+		 if( score < 60){
+
+			backgroundChanger('F.jpg');
+
+			return;
+			
+		}
+
+	}
+
+	function gameInfoPopulation(){
+
+		
+
+		for(var i = 0; i < (game.incorrect+game.correct); i++){
+
+			$('.tableBody').append($('<tr class="tableRemove tableRow'+ i + '"></tr>'));
+
+			$('.tableRow'+ i).append('<td class="tableRemove">' + game.questionsArray[i].questionNumber + '</td>');
+
+			$('.tableRow'+ i).append('<td class="tableRemove">' + game.questionsArray[i].question + '</td>');
+
+			$('.tableRow'+ i).append('<td class="tableRemove">' + game.questionsArray[i].right + '</td>');
+
+			$('.tableRow'+ i).append('<td class="tableRemove">' + game.userAnswers[i] + '</td>');
+
+			if(game.userAnswers[i] == game.questionsArray[i].right){
+
+				$('.tableRow'+ i).append('<td class="tableRemove"><i class="fa fa-check-circle" aria-hidden="true"></i></td>');
+
+			}
+
+			else {
+
+				$('.tableRow'+ i).append('<td class="tableRemove"><i class="fa fa-times-circle" aria-hidden="true"></i></td>');
+
+			}
+
+		}
+
+		$('.gameInfoCol').append('<p class="tableRemove">Number Of Correct Answers : ' + game.correct + '<br>' + 'Number Of Incorrect Answers : ' + game.incorrect +'</p>');
+
+	}
+
 	function populateQuestion(){
+
+		if(game.time == -1){
+
+			game.time = 15;
+
+			htmlTimer();
+
+		}
+
+		if($('.modal').css('display') == 'block'){
+
+			$('.modal').css('display', 'none');
+
+			clearInterval(game.gameStart);
+
+			htmlTimer();
+		}
 
 		if(game.questionsLeft == game.numberOfQuestions){
 
-			alert('You finished');
+			clearInterval(game.gameStart);
+
+			clearInterval(game.timer);
+
+			$('.questionScreen').css('display', 'none');
+
+			$('.finishScreen').css('display', 'block');
+
+			gameInfoPopulation();
+
+			scoreGiver();
+
+			
+
+			return;
 
 		}
+
+		game.correctAnswer = game.questionsArray[game.numberOfQuestions].right;
 
 		$('.questionNumberHolder').html('<p> Question # ' + game.questionsArray[game.numberOfQuestions].questionNumber + '</p>');
 
@@ -211,7 +364,41 @@ $(document).ready(function(){
 
 		game.numberOfQuestions++;
 
-		game.gameStart = setInterval(populateQuestion, 15000);
+	}
+
+	function htmlTimer(){
+
+		if(game.time == -1){
+
+			game.time = 15;
+
+			game.userAnswers.push('No Answer');
+
+			clearInterval(game.timer);
+
+			game.incorrect++;
+
+			$('.modal').css('display', 'block');
+
+			$('.modalAnswer').html('You ran out of time! The correct answer was ' + game.correctAnswer + '. `-`');
+
+			game.gameStart = setInterval(populateQuestion, 4000);
+
+			return;
+
+		}
+
+		$('.timeRemainingHolder').html('<p> Time Remaining: ' + game.time + '</p>')
+
+		if(game.timer != ''){
+
+			clearInterval(game.timer);
+
+		}
+
+		game.time--;
+
+		game.timer = setInterval(htmlTimer,1000);
 	}
 
 	//The button with the class ' .begin' is initially hidden, after the game mode is set, it's display changes to ' block'. 
@@ -234,18 +421,18 @@ $(document).ready(function(){
 
 		for(var i = 0; i < allQuestions.length; i++){
 
-			if(allQuestions[i].difficulty == game.difficulty && allQuestions[i].category == game.category && game.numberOfQuestions > 0){
+			if(allQuestions[i].difficulty == game.difficulty && allQuestions[i].category == game.category && game.maxQuestions > 0){
 
-				game.questionsArray.push(allQuestions[i]);
+				game.toBeAdded.push(allQuestions[i]);
 
-				game.numberOfQuestions--;
+				game.maxQuestions--;
 
 			}
 
 			//Because numberOfQuestions is decreased everytime the questionsArray is populated, when numberOfQuestions reaches 0 it will have selected all the
 			//pertaining questions 
 
-			if(game.numberOfQuestions == 0){
+			if(game.maxQuestions == 0){
 
 				break;
 
@@ -253,26 +440,109 @@ $(document).ready(function(){
 
 		}
 
+		for(var j = 0; j < game.numberOfQuestions; j++){
+
+			var random = Math.floor(Math.random()*game.toBeAdded.length);
+
+			game.toBeAdded[random].questionNumber = j + 1;
+
+			game.questionsArray.push(game.toBeAdded[random]);
+
+			game.toBeAdded.splice(random, 1);
+
+		}
+
+		game.numberOfQuestions = 0;
+
 		game.questionsLeft = game.questionsArray.length;
 
 		populateQuestion();
+
+		htmlTimer();
 
 	});
 
 	$('.answer').on('click', function(){
 
+		clearInterval(game.timer);
+
+		game.time = 15;
+
+		game.userAnswers.push($(this).attr('data-answer'));
+
 		if($(this).attr('data-answer') == $(this).attr('data-rightAnswer')){
 
-			alert( 'correct');
+			game.correct++;
+
+			$('.modal').css('display', 'block');
+
+			$('.modalAnswer').html('Congratulations! The correct answer is ' + $(this).attr('data-rightAnswer') + '. =]');
+
+			game.gameStart = setInterval(populateQuestion, 4000);
+
+		} 
+
+		else {
+
+			game.incorrect++;
+
+			$('.modal').css('display', 'block');
+
+			$('.modalAnswer').html('Opps! The correct answer was ' + $(this).attr('data-rightAnswer') + '. :(');
+
+			game.gameStart = setInterval(populateQuestion, 4000);
 
 		}
 
 	});
 
+	$('.playAgain').on('click', function(){
+
+		$('.finishScreen').css('display', 'none');
+
+		$('.gameSelectionScreen').css('display', 'block');
+
+		backgroundChanger('gameshow.jpg');
+
+		$('.begin').css('display', 'none');
+
+		game.difficulty  = '';
+
+		game.category  = '';
+
+		game.numberOfQuestions = 0;
+
+		game.time = 15;
+
+		game.timer ='';
+
+		game.questionsLeft = 0;
+
+		game.gameStart ='';
+
+		game.questionsArray = [];
+
+		game.correct = 0;
+
+		game.incorrect = 0;
+
+		game.correctAnswer =' ';
+
+		game.userAnswers = [];
+
+		game.toBeAdded = [];
+
+		game.maxQuestions =  15;
+
+		$('.difficultyCheck').remove();
+
+		$('.categoryCheck').remove();
+
+		$('.questionCheck').remove();
+
+		$('.tableRemove').remove();
 
 
-
-
-
+	});
 
 });
